594928dcd5bfb2fde07ba5cba24bd771498bd313
stephdin <stephdin@users.noreply.github.com>
2023-08-07 02:01:05 +0200

    add basic interaction for activation of text editor by clicking

--- src/components/App/TextEditor.tsx
+++ src/components/App/TextEditor.tsx
@@ -1,16 +1,20 @@
-import { RichTextEditor, Link } from "@mantine/tiptap";
+import { Button, Group } from "@mantine/core";
+import { Link, RichTextEditor } from "@mantine/tiptap";
+import Highlight from "@tiptap/extension-highlight";
+import SubScript from "@tiptap/extension-subscript";
+import Superscript from "@tiptap/extension-superscript";
+import Underline from "@tiptap/extension-underline";
 import { useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-import Underline from "@tiptap/extension-underline";
-import Superscript from "@tiptap/extension-superscript";
-import SubScript from "@tiptap/extension-subscript";
-import Highlight from "@tiptap/extension-highlight";
+import { useEffect, useState } from "react";
 
 interface TextEditorProps {
   content: string;
 }
 
 export function TextEditor({ content }: TextEditorProps) {
+  const [isEditable, setIsEditable] = useState(false);
+
   const editor = useEditor({
     extensions: [
       StarterKit,
@@ -21,37 +25,69 @@ export function TextEditor({ content }: TextEditorProps) {
       Highlight,
     ],
     content,
+    editable: isEditable,
   });
 
+  useEffect(() => {
+    editor?.setEditable(isEditable);
+    if (isEditable) {
+      editor?.commands.focus();
+    }
+  }, [isEditable]);
+
   return (
-    <RichTextEditor editor={editor}>
-      <RichTextEditor.Toolbar sticky stickyOffset={60}>
-        <RichTextEditor.ControlsGroup>
-          <RichTextEditor.Bold />
-          <RichTextEditor.Italic />
-          <RichTextEditor.Underline />
-          <RichTextEditor.Strikethrough />
-          <RichTextEditor.ClearFormatting />
-          <RichTextEditor.Highlight />
-          <RichTextEditor.Code />
-        </RichTextEditor.ControlsGroup>
+    <>
+      <RichTextEditor
+        editor={editor}
+        onClick={() => setIsEditable(true)}
+        // style={!isEditable ? { border: "none", marginTop: 48 } : {}}
+      >
+        {isEditable && (
+          <RichTextEditor.Toolbar>
+            <RichTextEditor.ControlsGroup>
+              <RichTextEditor.Bold />
+              <RichTextEditor.Italic />
+              <RichTextEditor.Underline />
+              <RichTextEditor.Strikethrough />
+              <RichTextEditor.ClearFormatting />
+              <RichTextEditor.Highlight />
+              <RichTextEditor.Code />
+            </RichTextEditor.ControlsGroup>
 
-        <RichTextEditor.ControlsGroup>
-          <RichTextEditor.Blockquote />
-          <RichTextEditor.Hr />
-          <RichTextEditor.BulletList />
-          <RichTextEditor.OrderedList />
-          <RichTextEditor.Subscript />
-          <RichTextEditor.Superscript />
-        </RichTextEditor.ControlsGroup>
+            <RichTextEditor.ControlsGroup>
+              <RichTextEditor.Blockquote />
+              <RichTextEditor.Hr />
+              <RichTextEditor.BulletList />
+              <RichTextEditor.OrderedList />
+              <RichTextEditor.Subscript />
+              <RichTextEditor.Superscript />
+            </RichTextEditor.ControlsGroup>
 
-        <RichTextEditor.ControlsGroup>
-          <RichTextEditor.Link />
-          <RichTextEditor.Unlink />
-        </RichTextEditor.ControlsGroup>
-      </RichTextEditor.Toolbar>
+            <RichTextEditor.ControlsGroup>
+              <RichTextEditor.Link />
+              <RichTextEditor.Unlink />
+            </RichTextEditor.ControlsGroup>
+          </RichTextEditor.Toolbar>
+        )}
 
-      <RichTextEditor.Content />
-    </RichTextEditor>
+        <RichTextEditor.Content />
+      </RichTextEditor>
+
+      {isEditable && (
+        <Group>
+          <Button
+            variant="subtle"
+            color="gray"
+            onClick={() => setIsEditable(false)}
+          >
+            Abbrechen
+          </Button>
+
+          <Button variant="default" onClick={() => setIsEditable(false)}>
+            Speichern
+          </Button>
+        </Group>
+      )}
+    </>
   );
 }
