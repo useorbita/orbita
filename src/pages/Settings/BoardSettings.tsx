7112a0354aaa2ab85e350fb6db0a41ff074c4c5e
stephdin <stephdin@users.noreply.github.com>
2023-09-08 23:44:38 +0200

    add dummy board settings page

--- src/pages/Settings/BoardSettings.tsx
+++ src/pages/Settings/BoardSettings.tsx
@@ -1,10 +1,24 @@
-import { ActionIcon, Group, Text } from "@mantine/core";
+import {
+  ActionIcon,
+  Button,
+  Grid,
+  Group,
+  MultiSelect,
+  Space,
+  Stack,
+  Text,
+  TextInput,
+} from "@mantine/core";
 import { IconArrowLeft } from "@tabler/icons-react";
 import { useNavigate, useParams } from "react-router-dom";
 
 export function BoardSettings() {
   const navigate = useNavigate();
   const { boardId } = useParams();
+
+  const descriptionSpan = 4;
+  const inputSpan = 6;
+  const offset = 1;
 
   return (
     <>
@@ -18,16 +32,65 @@ export function BoardSettings() {
         >
           <IconArrowLeft size="1em" />
         </ActionIcon>
-        <Text>{boardId}</Text>
+        <Text>{boardId} Einstellungen</Text>
       </Group>
 
-      <ul>
-        <li>Name</li>
-        <li>Mitglieder</li>
-        <li>Board löschen</li>
-        <li>Labels (Titel, Farbe)</li>
-        <li>States (Titel)</li>
-      </ul>
+      <Space h={"xl"} />
+
+      <Grid align="flex-start">
+        <Grid.Col span={descriptionSpan}>
+          <Stack gap={"xs"}>
+            <Text>Name des Boards</Text>
+            <Text size="sm" c="dimmed">
+              Dies ist der Name des Boards der jedem angezeigt wird
+            </Text>
+          </Stack>
+        </Grid.Col>
+        <Grid.Col span={inputSpan} offset={offset}>
+          <TextInput placeholder="Name des Boards" />
+        </Grid.Col>
+      </Grid>
+
+      <Space h={"xl"} />
+
+      <Grid align="flex-start">
+        <Grid.Col span={descriptionSpan}>
+          <Stack gap={"xs"}>
+            <Text>Mitglieder</Text>
+            <Text size="sm" c="dimmed">
+              Folgende Mitglieder haben Zugriff auf das Board
+            </Text>
+          </Stack>
+        </Grid.Col>
+        <Grid.Col span={inputSpan} offset={offset}>
+          <MultiSelect
+            placeholder="Auswählen"
+            data={["Max", "Erika", "Jane", "John"]}
+          />
+        </Grid.Col>
+      </Grid>
+
+      <Space h={"xl"} />
+
+      <Text>- Label</Text>
+      <Text>- States</Text>
+
+      <Space h={"xl"} />
+
+      <Grid align="flex-start">
+        <Grid.Col span={descriptionSpan}>
+          <Stack gap={"xs"}>
+            <Text>Board löschen</Text>
+            <Text size="sm" c="dimmed">
+              Das Board wird und alle dazugehörigen Daten werden gelöscht. Dies
+              kann nicht rückgängig gemacht werden
+            </Text>
+          </Stack>
+        </Grid.Col>
+        <Grid.Col span={inputSpan} offset={offset}>
+          <Button color={"red"}>Board löschen</Button>
+        </Grid.Col>
+      </Grid>
     </>
   );
 }
