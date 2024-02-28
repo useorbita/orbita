42ccc390b3b6289a6f3a7d4c3f3a316cf59ad451
stephdin <stephdin@users.noreply.github.com>
2024-02-28 23:41:07 +0100

    update dependencies

--- pocketbase/install.sh
+++ pocketbase/install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-VERSION=0.21.1
+VERSION=0.22.0
 
 echo Downloading PocketBase v$VERSION...
 wget --quiet "https://github.com/pocketbase/pocketbase/releases/download/v${VERSION}/pocketbase_${VERSION}_linux_amd64.zip"
